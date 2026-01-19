748300435c71dc3167ce0f60e2babb4fa114fee9
Pawel Wujczyk <pwujczyk@gmail.com>
2026-01-19 23:18:43 +0100

    reference

--- Function.cs
+++ Function.cs
@@ -1,20 +1,23 @@
+using System;
+using System.Collections.Generic;
 using Google.Cloud.Functions.Framework;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace ProductivityTools.GetTask3.Reporting;
 
 public class Function : IHttpFunction
 {
-    /// <summary>
-    /// Logic for your function goes here.
-    /// </summary>
-    /// <param name="context">The HTTP context, containing the request and the response.</param>
-    /// <returns>A task representing the asynchronous operation.</returns>
+    static string URL = "https://apigettask3.productivitytools.top:8042/api/";// Consts.EndpointAddress;
+
     public async Task HandleAsync(HttpContext context)
     {
 
-        string s = await GetClosedForLast7Days(log);
+        Action<string> consoleLog = (s) => Console.WriteLine(s);
+
+        string s = await GetClosedForLast7Days(consoleLog);
         // SendEmail(s, log);
         // s = await GetClosedForThisWeek(log);
         // SendEmail(s, log);
@@ -24,10 +27,130 @@ public class Function : IHttpFunction
         await context.Response.WriteAsync("Hello, Functions Framework.", context.RequestAborted);
     }
 
-    private static async Task<string> GetClosedForLast7Days(ILogger log)
+    private static IConfigurationRoot Configuration
     {
-        Action<string> lg = (s) => log.LogInformation(s);
-        var rootElement = await new ProductivityTools.GetTask3.Sdk.TaskClient(URL, FirebaseWebApiKey, lg).GetThisWeekFinishedForUser(null, string.Empty, "pwujczyk@gmail.com");
+        get
+        {
+            var configuration = new ConfigurationBuilder()
+                   .AddMasterConfiguration("ProductivityTools.GetTask3.Client.json")
+                   .AddEnvironmentVariables()
+                   .Build();
+            return configuration;
+        }
+    }
+
+    private static string FirebaseWebApiKey
+    {
+        get
+        {
+            var key = Configuration["FirebaseWebApiKey"];
+            return key;
+        }
+    }
+
+    private static async Task<List<Contract.ElementView>> FindElements(Contract.ElementView root, List<string> names)
+    {
+        var result = new List<Contract.ElementView>();
+        foreach (var item in names)
+        {
+            var r1 = await FindElement(root, item);
+            result.AddRange(r1);
+        }
+        return result;
+    }
+
+    private static async Task<List<Contract.ElementView>> FindElement(Contract.ElementView root, string name)
+    {
+        var result = new List<Contract.ElementView>();
+        if (root.Name == name)
+        {
+            result.Add(root);
+        }
+        else
+        {
+            foreach (var el in root.Elements)
+            {
+
+                var temp = await FindElement(el, name);
+
+                result.AddRange(temp);
+
+            }
+        }
+        return result;
+    }
+
+    private static async Task<Contract.ElementView> FindElement(Contract.ElementView root, int id)
+    {
+        if (root.ElementId == id)
+        {
+            return root;
+        }
+        else
+        {
+            foreach (var el in root.Elements)
+            {
+
+                var temp = await FindElement(el, id);
+                if (temp != null)
+                {
+                    return temp;
+                }
+            }
+        }
+        return null;
+    }
+
+    private static async Task<string> GetPathToRoot(Contract.ElementView root, Contract.ElementView element)
+    {
+        string s = string.Empty;
+        while (element != null && element.ParentId != null)
+        {
+            s = s.Insert(0, element.Name + " >>");
+            element = await FindElement(root, element.ParentId.Value);
+        }
+        return s;
+    }
+
+    private static async Task<string> GetClosedForThisWeek(Action<string> log)
+    {
+        var rootElement = await new ProductivityTools.GetTask3.Sdk.TaskClient(URL, FirebaseWebApiKey, log).GetThisWeekFinishedForUser(null, string.Empty, "pwujczyk@gmail.com");
         return await GetClosed(log, rootElement);
     }
+
+    private static async Task<string> GetClosedForLast7Days(Action<string> log)
+    {
+        var rootElement = await new ProductivityTools.GetTask3.Sdk.TaskClient(URL, FirebaseWebApiKey, log).GetThisWeekFinishedForUser(null, string.Empty, "pwujczyk@gmail.com");
+        return await GetClosed(log, rootElement);
+    }
+
+    private static async Task<string> GetClosed(Action<string> log, Contract.ElementView rootElement)
+    {
+        log($"C# Timer trigger function executed at: {DateTime.Now}");
+
+        log("firebase weba pi key");
+        log(FirebaseWebApiKey);
+
+
+        //ReportMd.PrepareReport(rootElement);
+
+        //string result = ReportSimple.PrepareReport(rootElement);
+        var inbox = await FindElements(rootElement, new List<string> { "PawelPC", "Google" });
+        string result = string.Empty;
+        foreach (var i in inbox)
+        {
+            result += await GetPathToRoot(rootElement, i);
+            result += ReportMd.PrepareReport(i);
+        }
+        return result;
+    }
+
+    private static void SendEmail(string body, Action<string> log)
+    {
+        string password = Configuration["GmailPassword"];
+        log.LogInformation("gmail pass");
+        log.LogInformation(password);
+        SendEmailGmail.Gmail.Send("productivitytools.tech@gmail.com", password, "pwujczyk@gmail.com", "GetTask3", body);
+    }
+
 }
